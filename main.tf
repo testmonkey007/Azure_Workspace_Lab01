99e51667375b510ecb2a94490352ccdb3334e93e
Test Monkey <testmonkey@home.ca>
2025-01-26 13:52:44 -0500

    Commit 3

--- main.tf
+++ main.tf
@@ -11,13 +11,13 @@ terraform {
 variable "AWS_ACCESS_KEY_ID" {
   description = "AWS Access Key ID"
   type        = string
-  sensitive   = true
+  sensitive   = false
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
   description = "AWS Secret Access Key"
   type        = string
-  sensitive   = true
+  sensitive   = false
 }
 
 provider "aws" {
